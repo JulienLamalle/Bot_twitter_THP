6e3a7dab2b3277404879a0a14e12d6ef4809ebb3
Julien Lamalle <eparis@atlassian.com>
2020-07-14 19:45:43 +0200

    I improve my first bot

--- lib/bot_twi.rb
+++ lib/bot_twi.rb
@@ -1,5 +1,14 @@
+require 'twitter'
 require 'dotenv'
 
 Dotenv.load('.env') 
 
-puts ENV['TWITTER_API_SECRET']
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+end
+
